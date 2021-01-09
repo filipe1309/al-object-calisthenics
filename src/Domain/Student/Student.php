f2720c51753da620630c22b869a61a34d38e83db
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-09 14:45:49 -0300

    refactor: change abstract level of hasAcess method

--- src/Domain/Student/Student.php
+++ src/Domain/Student/Student.php
@@ -67,16 +67,21 @@ class Student
     public function hasAccess(): bool
     {
         if ($this->watchedVideos->count() > 0) {
-            $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
-            /** @var DateTimeInterface $firstDate */
-            $firstDate = $this->watchedVideos->first()->value;
-            $today = new \DateTimeImmutable();
+            return $this->fisrtVideoWasWatchedinLessThan90days();
+        }
+        
+        return true;
+    }
 
-            if ($firstDate->diff($today)->days >= 90) {
-                return false;
-            } else {
-                return true;
-            }
+    public function fisrtVideoWasWatchedinLessThan90days(): bool
+    {
+        $this->watchedVideos->sort(fn (DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
+        /** @var DateTimeInterface $firstDate */
+        $firstDate = $this->watchedVideos->first()->value;
+        $today = new \DateTimeImmutable();
+
+        if ($firstDate->diff($today)->days >= 90) {
+            return false;
         } else {
             return true;
         }
